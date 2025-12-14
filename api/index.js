00a3bc00e79ce389e55b7cca64a6c1fe4bfbf215
Stephen <smpain@gmail.com>
2025-12-14 18:02:23 +0000

    Fix static file path in api/index.js to serve files from root directory

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('.'));
+app.use(express.static(path.join(__dirname, '..')));
 
 const questions = questionsData.allQuestions;
 
